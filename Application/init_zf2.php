a3d331cf6f833bc46eca2009d74cdcdd3f4203f7
DaemonAlchemist <andy@wittrock.us>
2013-11-04 00:09:03 -0600

    Move app config into ATP

--- Application/init_zf2.php
+++ Application/init_zf2.php
@@ -3,8 +3,11 @@
 include 'vendor/ZF2/library/Zend/Loader/AutoloaderFactory.php';
 Zend\Loader\AutoloaderFactory::factory(array(
 	'Zend\Loader\StandardAutoloader' => array(
-		'autoregister_zf' => true
+		'autoregister_zf' => true,
+		'namespaces' => array(
+			'ATP' => 'vendor/ATP',
+    )
 	)
 ));
 
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+Zend\Mvc\Application::init(require 'vendor/ATP/configs/application.config.php')->run();
